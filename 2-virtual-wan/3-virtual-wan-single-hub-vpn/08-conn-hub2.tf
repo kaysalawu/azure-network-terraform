740c253b31696b7287b6b2175a0eb9bf6b3272f0
Kayode Salawu <salawu@gmail.com>
2023-01-22 14:51:48 +0000

    version v1.0

---
+++ 2-virtual-wan/3-virtual-wan-single-hub-vpn/08-conn-hub2.tf
@@ -0,0 +1,282 @@
+
+####################################################
+# vnet peering
+####################################################
+
+# spoke5-to-hub2
+
+resource "azurerm_virtual_network_peering" "spoke5_to_hub2_peering" {
+  resource_group_name          = azurerm_resource_group.rg.name
+  name                         = "${local.prefix}-spoke5-to-hub2-peering"
+  virtual_network_name         = module.spoke5.vnet.name
+  remote_virtual_network_id    = module.hub2.vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  use_remote_gateways          = true
+  depends_on = [
+    module.hub2.vpngw
+  ]
+}
+
+# hub2-to-spoke5
+
+resource "azurerm_virtual_network_peering" "hub2_to_spoke5_peering" {
+  resource_group_name          = azurerm_resource_group.rg.name
+  name                         = "${local.prefix}-hub2-to-spoke5-peering"
+  virtual_network_name         = module.hub2.vnet.name
+  remote_virtual_network_id    = module.spoke5.vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = true
+  depends_on = [
+    module.hub2.vpngw
+  ]
+}
+
+####################################################
+# nva
+####################################################
+
+locals {
+  hub2_router_route_map_name_nh = "NEXT-HOP"
+  hub2_router_init = templatefile("../../scripts/nva-hub.sh", {
+    LOCAL_ASN = local.hub2_nva_asn
+    LOOPBACK0 = local.hub2_nva_loopback0
+    LOOPBACKS = {
+      Loopback1 = local.hub2_nva_ilb_addr
+    }
+    INT_ADDR = local.hub2_nva_addr
+    VPN_PSK  = local.psk
+
+    ROUTE_MAPS = [
+      {
+        name   = local.hub2_router_route_map_name_nh
+        action = "permit"
+        rule   = 100
+        commands = [
+          "match ip address prefix-list all",
+          "set ip next-hop ${local.hub2_nva_ilb_addr}"
+        ]
+      }
+    ]
+
+    TUNNELS = []
+
+    STATIC_ROUTES = [
+      { network = "0.0.0.0", mask = "0.0.0.0", next_hop = local.hub2_default_gw_nva },
+    ]
+
+    BGP_SESSIONS            = []
+    BGP_ADVERTISED_NETWORKS = []
+  })
+}
+
+module "hub2_nva" {
+  source               = "../../modules/csr-hub"
+  resource_group       = azurerm_resource_group.rg.name
+  name                 = "${local.hub2_prefix}nva"
+  location             = local.hub2_location
+  enable_ip_forwarding = true
+  enable_public_ip     = true
+  subnet               = module.hub2.subnets["${local.hub2_prefix}nva"].id
+  private_ip           = local.hub2_nva_addr
+  storage_account      = azurerm_storage_account.region2
+  admin_username       = local.username
+  admin_password       = local.password
+  custom_data          = base64encode(local.hub2_router_init)
+}
+
+####################################################
+# udr
+####################################################
+
+# spoke5
+#----------------------------
+
+# route table
+
+resource "azurerm_route_table" "rt_spoke5" {
+  resource_group_name = azurerm_resource_group.rg.name
+  name                = "${local.prefix}-rt-spoke5"
+  location            = local.region2
+
+  disable_bgp_route_propagation = false
+}
+
+# udr
+
+locals {
+  rt_spoke5_routes = {
+    spoke5 = "10.0.0.0/8"
+  }
+}
+
+resource "azurerm_route" "spoke5_routes_hub2" {
+  for_each               = local.rt_spoke5_routes
+  name                   = "${local.prefix}-${each.key}-route-hub2"
+  resource_group_name    = azurerm_resource_group.rg.name
+  route_table_name       = azurerm_route_table.rt_spoke5.name
+  address_prefix         = each.value
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = local.hub2_nva_addr
+}
+
+# association
+
+resource "azurerm_subnet_route_table_association" "spoke5_routes_hub2" {
+  subnet_id      = module.spoke5.subnets["${local.spoke5_prefix}main"].id
+  route_table_id = azurerm_route_table.rt_spoke5.id
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+####################################################
+# vpn-site connection
+####################################################
+
+# branch3
+
+resource "azurerm_vpn_gateway_connection" "vhub2_site_branch3_conn" {
+  name                      = "${local.vhub2_prefix}site-branch3-conn"
+  vpn_gateway_id            = azurerm_vpn_gateway.vhub2.id
+  remote_vpn_site_id        = azurerm_vpn_site.vhub2_site_branch3.id
+  internet_security_enabled = false
+
+  vpn_link {
+    name             = "${local.vhub2_prefix}site-branch3-conn-vpn-link-0"
+    bgp_enabled      = true
+    shared_key       = local.psk
+    vpn_site_link_id = azurerm_vpn_site.vhub2_site_branch3.link[0].id
+  }
+
+  routing {
+    associated_route_table = azurerm_virtual_hub.vhub2.default_route_table_id
+    propagated_route_table {
+      labels = [
+        "default",
+      ]
+      route_table_ids = [
+        azurerm_virtual_hub.vhub2.default_route_table_id,
+      ]
+    }
+  }
+}
+
+# hub2
+
+resource "azurerm_vpn_gateway_connection" "vhub2_site_hub2_conn" {
+  name                      = "${local.vhub2_prefix}site-hub2-conn"
+  vpn_gateway_id            = azurerm_vpn_gateway.vhub2.id
+  remote_vpn_site_id        = azurerm_vpn_site.vhub2_site_hub2.id
+  internet_security_enabled = false
+
+  vpn_link {
+    name             = "${local.vhub2_prefix}site-hub2-conn-vpn-link-0"
+    bgp_enabled      = true
+    shared_key       = local.psk
+    vpn_site_link_id = azurerm_vpn_site.vhub2_site_hub2.link[0].id
+  }
+  vpn_link {
+    name             = "${local.vhub2_prefix}site-hub2-conn-vpn-link-1"
+    bgp_enabled      = true
+    shared_key       = local.psk
+    vpn_site_link_id = azurerm_vpn_site.vhub2_site_hub2.link[1].id
+  }
+
+  routing {
+    associated_route_table = azurerm_virtual_hub.vhub2.default_route_table_id
+    propagated_route_table {
+      labels = [
+        "default",
+      ]
+      route_table_ids = [
+        azurerm_virtual_hub.vhub2.default_route_table_id,
+      ]
+    }
+  }
+}
+
+####################################################
+# vhub connections
+####################################################
+
+# spoke4
+
+resource "azurerm_virtual_hub_connection" "spoke4_vnet_conn" {
+  name                      = "${local.vhub2_prefix}spoke4-vnet-conn"
+  virtual_hub_id            = azurerm_virtual_hub.vhub2.id
+  remote_virtual_network_id = module.spoke4.vnet.id
+
+  routing {
+    associated_route_table_id = azurerm_virtual_hub.vhub2.default_route_table_id
+    propagated_route_table {
+      labels = [
+        "default",
+      ]
+      route_table_ids = [
+        azurerm_virtual_hub.vhub2.default_route_table_id,
+      ]
+    }
+  }
+}
+
+####################################################
+# hub2
+####################################################
+
+# lng
+#----------------------------
+
+# vhub2
+
+resource "azurerm_local_network_gateway" "hub2_vhub2_lng0" {
+  resource_group_name = azurerm_resource_group.rg.name
+  name                = "${local.hub2_prefix}vhub2-lng0"
+  location            = local.hub2_location
+  gateway_address     = local.vhub2_vpngw_pip0
+  address_space       = ["${local.vhub2_vpngw_bgp0}/32", ]
+  bgp_settings {
+    asn                 = local.vhub2_bgp_asn
+    bgp_peering_address = local.vhub2_vpngw_bgp0
+  }
+}
+
+resource "azurerm_local_network_gateway" "hub2_vhub2_lng1" {
+  resource_group_name = azurerm_resource_group.rg.name
+  name                = "${local.hub2_prefix}vhub2-lng1"
+  location            = local.hub2_location
+  gateway_address     = local.vhub2_vpngw_pip1
+  address_space       = ["${local.vhub2_vpngw_bgp1}/32", ]
+  bgp_settings {
+    asn                 = local.vhub2_bgp_asn
+    bgp_peering_address = local.vhub2_vpngw_bgp1
+  }
+}
+
+# lng connection
+#----------------------------
+
+# vhub2
+
+resource "azurerm_virtual_network_gateway_connection" "hub2_vhub2_lng0" {
+  resource_group_name        = azurerm_resource_group.rg.name
+  name                       = "${local.hub2_prefix}vhub2-lng0"
+  location                   = local.hub2_location
+  type                       = "IPsec"
+  enable_bgp                 = true
+  virtual_network_gateway_id = module.hub2.vpngw.id
+  local_network_gateway_id   = azurerm_local_network_gateway.hub2_vhub2_lng0.id
+  shared_key                 = local.psk
+}
+
+resource "azurerm_virtual_network_gateway_connection" "hub2_vhub2_lng1" {
+  resource_group_name        = azurerm_resource_group.rg.name
+  name                       = "${local.hub2_prefix}vhub2-lng1"
+  location                   = local.hub2_location
+  type                       = "IPsec"
+  enable_bgp                 = true
+  virtual_network_gateway_id = module.hub2.vpngw.id
+  local_network_gateway_id   = azurerm_local_network_gateway.hub2_vhub2_lng1.id
+  shared_key                 = local.psk
+}
---
+++ modules/debian/main.tf
@@ -0,0 +1,100 @@
+
+locals {
+  prefix = var.prefix == "" ? "${var.name}" : join("-", [var.prefix, var.name, ""])
+}
+
+
+# public ip
+
+resource "azurerm_public_ip" "this" {
+  count               = var.enable_public_ip == true ? 1 : 0
+  resource_group_name = var.resource_group
+  name                = "${local.prefix}pip"
+  location            = var.location
+  sku                 = "Standard"
+  allocation_method   = "Static"
+  tags                = var.tags
+}
+
+# interface
+
+resource "azurerm_network_interface" "this" {
+  resource_group_name  = var.resource_group
+  name                 = "${local.prefix}nic"
+  location             = var.location
+  dns_servers          = var.dns_servers
+  tags                 = var.tags
+  enable_ip_forwarding = var.enable_ip_forwarding
+  ip_configuration {
+    name                          = "${local.prefix}nic-ip-config"
+    subnet_id                     = var.subnet
+    private_ip_address_allocation = var.private_ip == null ? "Dynamic" : "Static"
+    private_ip_address            = var.private_ip == null ? null : var.private_ip
+    public_ip_address_id          = var.enable_public_ip ? azurerm_public_ip.this.0.id : null
+  }
+}
+
+# vm
+
+resource "azurerm_linux_virtual_machine" "this" {
+  resource_group_name = var.resource_group
+  name                = trimsuffix(local.prefix, "-")
+  location            = var.location
+  zone                = var.zone
+  size                = var.vm_size
+  tags                = var.tags
+  custom_data         = var.use_vm_extension ? null : var.custom_data
+  network_interface_ids = [
+    azurerm_network_interface.this.id
+  ]
+  os_disk {
+    name                 = "${local.prefix}os-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Debian"
+    offer     = "debian-10"
+    sku       = "10"
+    version   = "0.20201013.422"
+  }
+  computer_name  = trimsuffix(local.prefix, "-")
+  admin_username = var.admin_username
+  admin_password = var.admin_password
+  boot_diagnostics {
+    storage_account_uri = var.storage_account.primary_blob_endpoint
+  }
+  disable_password_authentication = false
+
+  lifecycle {
+    ignore_changes = [
+      identity,
+      secure_boot_enabled,
+      tags,
+    ]
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "this" {
+  count                = var.use_vm_extension ? 1 : 0
+  name                 = trimsuffix(local.prefix, "-")
+  virtual_machine_id   = azurerm_linux_virtual_machine.this.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+{
+  "script": "${var.custom_data}"
+}
+SETTINGS
+}
+
+resource "azurerm_private_dns_a_record" "this" {
+  count               = var.private_dns_zone == "" ? 0 : 1
+  resource_group_name = var.resource_group
+  name                = lower(var.name)
+  zone_name           = var.private_dns_zone
+  ttl                 = 300
+  records             = [azurerm_linux_virtual_machine.this.private_ip_address]
+}
