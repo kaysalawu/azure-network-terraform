c0226d72689ae62cbbd8b57533edf9943e441061
Kayode Salawu <salawu@gmail.com>
2023-11-22 20:27:57 +0000

    update

--- helpers/_destroy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-terraform_destroy() {
-  terraform init
-  terraform destroy -auto-approve -lock=false -parallelism=50
-}
-
-time terraform_destroy
--- helpers/scripts/prepare.sh
+++ helpers/scripts/prepare.sh
@@ -48,15 +48,47 @@ done
 }
 
 delete_files(){
+  run_diff
+  diff_result=$?
+
+  # If run_diff returns 1, exit
+  if [ "$diff_result" -eq 1 ]; then
+      echo "Differences found, aborting delete." && echo
+      return
+  fi
+
   for file in "${files[@]}"; do
     local_file=$(basename "$file")
     if [ -e "$local_file" ]; then
-        rm "$local_file"
+        #rm "$local_file"
         echo "deleted: $local_file"
     else
         echo "skip: $local_file (not found)"
     fi
   done
+}
+
+run_diff() {
+    local all_diffs_ok=true
+    echo
+    for file in "${files[@]}"; do
+        local_file=$(basename "$file")
+        if [ -e "$local_file" ]; then
+            diff "$local_file" "$file" > /dev/null 2>&1
+            diff_exit_status=$?
+
+            if [ "$diff_exit_status" -ne 0 ]; then
+                echo "diff: $diff_exit_status --> $local_file"
+                all_diffs_ok=false
+            fi
+        else
+            echo "no local: $local_file"
+        fi
+    done
+
+    if [ "$all_diffs_ok" = false ]; then
+        return 1
+    fi
 }
 
 if [[ "$1" == "--replace" || "$1" == "-r" ]]; then
@@ -69,8 +101,10 @@ elif [[ "$1" == "--copy" || "$1" == "-c" ]]; then
     copy_files
 elif [[ "$1" == "--delete-files" || "$1" == "-x" ]]; then
     delete_files
+elif [[ "$1" == "--diff" || "$1" == "-f" ]]; then
+    run_diff
 elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
-    echo "Usage: $0 {--replace|-r|--add|-a|--delete|-d|--copy|-c|--delete-files|-x}"
+    echo "Usage: $0 {--replace|-r|--add|-a|--delete|-d|--copy|-c|--delete-files|-x|--diff|-f}"
 else
-    echo "Usage: $0 {--replace|-r|--add|-a|--delete|-d|--copy|-c|--delete-files|-x}"
+    echo "Usage: $0 {--replace|-r|--add|-a|--delete|-d|--copy|-c|--delete-files|-x|--diff|-f}"
 fi
