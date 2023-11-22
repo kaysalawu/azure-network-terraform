680235639ec7c14da37057c7570bb356da32266f
Kayode Salawu <salawu@gmail.com>
2023-11-22 18:10:45 +0000

    diag gw log, r.i., and azapi

--- modules/virtual-hub/main.tf
+++ modules/virtual-hub/main.tf
@@ -26,6 +26,13 @@ resource "azurerm_virtual_hub" "this" {
   }
 }
 
+data "azurerm_virtual_hub_route_table" "default" {
+  resource_group_name = var.resource_group
+  name                = "defaultRouteTable"
+  virtual_hub_name    = azurerm_virtual_hub.this.name
+  depends_on          = [azurerm_virtual_hub.this, ]
+}
+
 # vpngw
 #----------------------------
 
@@ -105,11 +112,12 @@ resource "azurerm_firewall" "this" {
 # diagnostic setting
 
 resource "azurerm_monitor_diagnostic_setting" "azfw" {
-  count                      = var.security_config[0].create_firewall ? 1 : 0
-  name                       = "${local.prefix}azfw-diag"
-  target_resource_id         = azurerm_firewall.this[0].id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.azfw[0].id
-  storage_account_id         = azurerm_storage_account.azfw[0].id
+  count                          = var.security_config[0].create_firewall ? 1 : 0
+  name                           = "${local.prefix}azfw-diag"
+  target_resource_id             = azurerm_firewall.this[0].id
+  log_analytics_workspace_id     = azurerm_log_analytics_workspace.azfw[0].id
+  log_analytics_destination_type = "Dedicated"
+  #storage_account_id         = azurerm_storage_account.azfw[0].id
 
   dynamic "metric" {
     for_each = var.metric_categories_firewall
@@ -139,11 +147,51 @@ resource "azurerm_virtual_hub_routing_intent" "this" {
   virtual_hub_id = azurerm_virtual_hub.this.id
 
   dynamic "routing_policy" {
-    for_each = var.routing_policies
+    for_each = var.routing_policies["internet"] ? [1] : []
     content {
-      name         = routing_policy.value.name
-      destinations = routing_policy.value.destinations
+      name         = "Internet"
+      destinations = ["Internet"]
       next_hop     = azurerm_firewall.this[0].id
     }
   }
+
+  dynamic "routing_policy" {
+    for_each = var.routing_policies["private_traffic"] ? [1] : []
+    content {
+      name         = "PrivateTraffic"
+      destinations = ["PrivateTraffic"]
+      next_hop     = azurerm_firewall.this[0].id
+    }
+  }
+}
+
+# resource "azapi_resource" "hub_route_intent" {
+#   count     = var.enable_routing_intent ? 1 : 0
+#   name      = "${local.prefix}hub-ri-policy"
+#   parent_id = azurerm_virtual_hub.this.id
+#   type      = "Microsoft.Network/virtualHubs/routingIntent@2022-09-01"
+
+#   body = jsonencode({
+#     properties = {
+#       routingPolicies = [for routing_policy in var.routing_policies : {
+#         name         = routing_policy.name
+#         destinations = routing_policy.destinations
+#         nextHop      = azurerm_firewall.this[0].id
+#       }]
+#     }
+#   })
+# }
+
+# static routes
+#----------------------------
+
+resource "azurerm_virtual_hub_route_table_route" "this" {
+  for_each          = var.enable_routing_intent ? var.routing_policies.additional_prefixes : {}
+  route_table_id    = data.azurerm_virtual_hub_route_table.default.id
+  name              = each.key
+  destinations_type = "CIDR"
+  destinations      = each.value
+  next_hop_type     = "ResourceId"
+  next_hop          = azurerm_firewall.this[0].id
+  depends_on        = [azurerm_virtual_hub.this]
 }
