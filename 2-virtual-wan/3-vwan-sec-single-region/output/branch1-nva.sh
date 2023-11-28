71d1872b99edc571bdb4717cad080324efd6c04e
Kayode Salawu <salawu@gmail.com>
2023-11-28 16:24:19 +0000

    update

--- 2-virtual-wan/3-vwan-sec-single-region/output/branch1-nva.sh
+++ 2-virtual-wan/3-vwan-sec-single-region/output/branch1-nva.sh
@@ -10,11 +10,11 @@ proposal AZURE-IKE-PROPOSAL
 match address local 10.10.1.9
 !
 crypto ikev2 keyring AZURE-KEYRING
-peer 20.50.166.30
-address 20.50.166.30
+peer 20.126.245.99
+address 20.126.245.99
 pre-shared-key changeme
-peer 20.50.211.227
-address 20.50.211.227
+peer 20.126.245.37
+address 20.126.245.37
 pre-shared-key changeme
 peer 10.30.1.9
 address 10.30.1.9
@@ -22,8 +22,8 @@ pre-shared-key changeme
 !
 crypto ikev2 profile AZURE-IKE-PROPOSAL
 match address local 10.10.1.9
-match identity remote address 20.50.166.30 255.255.255.255
-match identity remote address 20.50.211.227 255.255.255.255
+match identity remote address 20.126.245.99 255.255.255.255
+match identity remote address 20.126.245.37 255.255.255.255
 match identity remote address 10.30.1.9 255.255.255.255
 authentication remote pre-share
 authentication local pre-share
@@ -44,7 +44,7 @@ ip address 10.10.10.1 255.255.255.252
 tunnel mode ipsec ipv4
 ip tcp adjust-mss 1350
 tunnel source 10.10.1.9
-tunnel destination 20.50.166.30
+tunnel destination 20.126.245.99
 tunnel protection ipsec profile AZURE-IPSEC-PROFILE
 !
 interface Tunnel1
@@ -52,7 +52,7 @@ ip address 10.10.10.5 255.255.255.252
 tunnel mode ipsec ipv4
 ip tcp adjust-mss 1350
 tunnel source 10.10.1.9
-tunnel destination 20.50.211.227
+tunnel destination 20.126.245.37
 tunnel protection ipsec profile AZURE-IPSEC-PROFILE
 !
 interface Tunnel2
