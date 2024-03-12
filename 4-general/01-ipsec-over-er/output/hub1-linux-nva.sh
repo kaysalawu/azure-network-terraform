e7125ebd9529d900d7d5a90a376d74e51dcf0a71
Kayode Salawu <salawu@gmail.com>
2024-03-12 20:41:59 +0000

    g08 poc validated

---
+++ 4-general/01-ipsec-over-er/output/hub1-linux-nva.sh
@@ -0,0 +1,233 @@
+#!/bin/sh
+
+apt-get -y update
+apt-get -y install sipcalc
+
+#########################################################
+# ip forwarding
+#########################################################
+
+# Enable IPv4 forwarding
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv4.conf.eth0.disable_xfrm=1
+sysctl -w net.ipv4.conf.eth0.disable_policy=1
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+
+# Disable ICMP redirects
+# sysctl -w net.ipv4.conf.all.send_redirects=0
+# sysctl -w net.ipv4.conf.all.accept_redirects=0
+# sysctl -w net.ipv4.conf.eth0.send_redirects=0
+# sysctl -w net.ipv4.conf.eth0.accept_redirects=0
+# echo "net.ipv4.conf.all.send_redirects=0" >> /etc/sysctl.conf
+# echo "net.ipv4.conf.all.accept_redirects=0" >> /etc/sysctl.conf
+# echo "net.ipv4.conf.eth0.send_redirects=0" >> /etc/sysctl.conf
+# echo "net.ipv4.conf.eth0.accept_redirects=0" >> /etc/sysctl.conf
+# sysctl -p
+
+#########################################################
+# route table for eth1 (trust interface)
+#########################################################
+
+ETH1_DGW=$(sipcalc eth1 | awk '/Usable range/ {print $4}')
+ETH1_MASK=$(ip addr show eth1 | awk '/inet / {print $2}' | cut -d'/' -f2)
+
+# eth1 routing
+echo "2 rt1" | sudo tee -a /etc/iproute2/rt_tables
+
+# ip rules
+#-----------------------------------------------------
+# ip rules tell the kernel which routing table to use.
+# all traffic from/to eth1 subnet should use rt1 for lookup;
+# an example is traffic to/from eth1 floating IP (load balcner VIP)
+# the subnet mask expands the default GW IP to the entire subnet
+ip rule add from $ETH1_DGW/$ETH1_MASK table rt1
+ip rule add to $ETH1_DGW/$ETH1_MASK table rt1
+
+# the azure user-defined routes will direct all vnet inbound traffic to eth1 (trust)
+# if destination is internal (RFC1918 and RFC6598), ip rule directs kernel to use rt1 for lookup; and then use the ip routes in rt1
+# if destination is internet (not RFC1918 and RFC6598), use the main routing table for lookup and exit via eth0 default gateway
+# ip rule add to 10.0.0.0/8 table rt1
+# ip rule add to 172.16.0.0/12 table rt1
+# ip rule add to 192.168.0.0/16 table rt1
+# ip rule add to 100.64.0.0/10 table rt1
+
+# ip routes
+#--------------------------------------------------
+# kernel is directed to rt1 for RFC1918 and RFC6598 destinations
+# the following default route is used for traffic forwarding via eth1
+# ip route add 10.0.0.0/8 via $ETH1_DGW dev eth1 table rt1
+# ip route add 172.16.0.0/12 via $ETH1_DGW dev eth1 table rt1
+# ip route add 192.168.0.0/16 via $ETH1_DGW dev eth1 table rt1
+# ip route add 100.64.0.0/10 via $ETH1_DGW dev eth1 table rt1
+
+# for traffic originating from azure platform to eth1 ...
+# rule "ip rule add to $ETH1_DGW/$ETH1_MASK table rt1" is used
+# this rule directs that rt1 should be used for lookup
+# the return traffic will use the following rt1 routes
+ip route add 168.63.129.16/32 via $ETH1_DGW dev eth1 table rt1
+# ip route add 169.254.169.254/32 via $ETH1_DGW dev eth1 table rt1
+
+# alternatively, all the static routes can be replaced by a single default route
+# ip route add default via $ETH1_DGW dev eth1 table rt1
+
+#########################################################
+# iptables
+#########################################################
+
+echo iptables-persistent iptables-persistent/autosave_v4 boolean false | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean false | debconf-set-selections
+apt-get -y install iptables-persistent
+
+# Permit flows on all chains (for testing only and not for production)
+iptables -P FORWARD ACCEPT
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+
+# Iptables rules
+iptables -t nat -A POSTROUTING -d 10.0.0.0/8 -j ACCEPT
+iptables -t nat -A POSTROUTING -d 172.16.0.0/12 -j ACCEPT
+iptables -t nat -A POSTROUTING -d 192.168.0.0/16 -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+# Save to IPTables file for persistence on reboot
+iptables-save > /etc/iptables/rules.v4
+
+#########################################################
+# packages
+#########################################################
+
+sudo apt-get update
+sudo apt-get install -y strongswan frr
+
+##  run the updates and ensure the packages are up to date and there is no new version available for the packages
+#apt-get -y update --fix-missing
+apt-get -y install tcpdump dnsutils traceroute tcptraceroute net-tools
+
+sed -i 's/bgpd=no/bgpd=yes/' /etc/frr/daemons
+sudo systemctl restart frr
+
+#########################################################
+# strongswan config
+#########################################################
+
+tee /etc/ipsec.conf <<EOF
+
+EOF
+
+tee /etc/ipsec.secrets <<EOF
+
+EOF
+
+sudo tee /etc/ipsec.d/ipsec-vti.sh <<'EOF'
+
+EOF
+chmod a+x /etc/ipsec.d/ipsec-vti.sh
+
+touch /var/log/ipsec-vti.log
+systemctl restart ipsec.service
+
+# #########################################################
+# # frr  config
+# #########################################################
+
+sudo tee /etc/frr/frr.conf <<EOF
+# 
+EOF
+
+sudo systemctl enable frr
+sudo systemctl restart frr
+
+#########################################################
+# test scripts
+#########################################################
+
+# ping-ip
+
+cat <<EOF > /usr/local/bin/ping-ip
+echo -e "\n ping ip ...\n"
+echo "branch1 - 10.10.0.5 -\$(ping -qc2 -W1 10.10.0.5 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')"
+echo "hub1    - 10.11.0.5 -\$(ping -qc2 -W1 10.11.0.5 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')"
+echo "spoke1  - 10.1.0.5 -\$(ping -qc2 -W1 10.1.0.5 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')"
+echo "spoke2  - 10.2.0.5 -\$(ping -qc2 -W1 10.2.0.5 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')"
+echo "internet - icanhazip.com -\$(ping -qc2 -W1 icanhazip.com 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')"
+EOF
+chmod a+x /usr/local/bin/ping-ip
+
+# ping-dns
+
+cat <<EOF > /usr/local/bin/ping-dns
+echo -e "\n ping dns ...\n"
+echo "branch1vm.corp - \$(dig +short branch1vm.corp | tail -n1) -\$(ping -qc2 -W1 branch1vm.corp 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')"
+echo "hub1vm.eu.az.corp - \$(dig +short hub1vm.eu.az.corp | tail -n1) -\$(ping -qc2 -W1 hub1vm.eu.az.corp 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')"
+echo "spoke1vm.eu.az.corp - \$(dig +short spoke1vm.eu.az.corp | tail -n1) -\$(ping -qc2 -W1 spoke1vm.eu.az.corp 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')"
+echo "spoke2vm.eu.az.corp - \$(dig +short spoke2vm.eu.az.corp | tail -n1) -\$(ping -qc2 -W1 spoke2vm.eu.az.corp 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')"
+echo "icanhazip.com - \$(dig +short icanhazip.com | tail -n1) -\$(ping -qc2 -W1 icanhazip.com 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')"
+EOF
+chmod a+x /usr/local/bin/ping-dns
+
+# curl-ip
+
+cat <<EOF > /usr/local/bin/curl-ip
+echo -e "\n curl ip ...\n"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null 10.10.0.5) - branch1 (10.10.0.5)"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null 10.11.0.5) - hub1    (10.11.0.5)"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null 10.1.0.5) - spoke1  (10.1.0.5)"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null 10.2.0.5) - spoke2  (10.2.0.5)"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null icanhazip.com) - internet (icanhazip.com)"
+EOF
+chmod a+x /usr/local/bin/curl-ip
+
+# curl-dns
+
+cat <<EOF > /usr/local/bin/curl-dns
+echo -e "\n curl dns ...\n"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null branch1vm.corp) - branch1vm.corp"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null hub1vm.eu.az.corp) - hub1vm.eu.az.corp"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null spoke3pls.eu.az.corp) - spoke3pls.eu.az.corp"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null spoke1vm.eu.az.corp) - spoke1vm.eu.az.corp"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null spoke2vm.eu.az.corp) - spoke2vm.eu.az.corp"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null icanhazip.com) - icanhazip.com"
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%{http_code} (%{time_total}s) - %{remote_ip}" -s -o /dev/null https://g01ipsecerspoke3sa4a2b.blob.core.windows.net/spoke3/spoke3.txt) - https://g01ipsecerspoke3sa4a2b.blob.core.windows.net/spoke3/spoke3.txt"
+EOF
+chmod a+x /usr/local/bin/curl-dns
+
+# trace-ip
+
+cat <<EOF > /usr/local/bin/trace-ip
+echo -e "\n trace ip ...\n"
+traceroute 10.10.0.5
+echo -e "branch1\n"
+traceroute 10.11.0.5
+echo -e "hub1   \n"
+traceroute 10.1.0.5
+echo -e "spoke1 \n"
+traceroute 10.2.0.5
+echo -e "spoke2 \n"
+traceroute icanhazip.com
+echo -e "internet\n"
+EOF
+chmod a+x /usr/local/bin/trace-ip
+
+# dns-info
+
+cat <<EOF > /usr/local/bin/dns-info
+echo -e "\n resolvectl ...\n"
+resolvectl status
+EOF
+chmod a+x /usr/local/bin/dns-info
+
+# ipsec debug
+
+cat <<EOF > /usr/local/bin/ipsec-debug
+echo -e "\n ============ ipsec statusall ============ \n"
+ipsec statusall
+echo -e "\n ============ ipsec status ============ \n"
+ipsec status
+echo -e "\n ============ ipsec-vti.log ============ \n"
+cat /var/log/ipsec-vti.log
+echo -e "\n ============ link vti ============ \n"
+sudo ip link show type vti
+echo
+EOF
+chmod a+x /usr/local/bin/ipsec-debug
