dd3c365645a88a786f1b039b4799f914e14e360a
Kayode Salawu <salawu@gmail.com>
2024-03-26 20:48:44 +0000

    update

--- scripts/init/crawler/app/find_subnet.py
+++ scripts/init/crawler/app/find_subnet.py
@@ -14,7 +14,7 @@ def get_ip_address():
     if process.returncode == 0:
         return stdout.decode('utf-8').strip()
     else:
-        raise Exception("Error getting eth0 IP address: " + stderr.decode('utf-8'))
+        raise Exception("Error getting IP address: " + stderr.decode('utf-8'))
 
 def get_vnet_subnets():
     cmd = f"az network vnet subnet list --resource-group {RESOURCE_GROUP} --vnet-name {VNET_NAME} -o json"
@@ -22,25 +22,33 @@ def get_vnet_subnets():
     stdout, stderr = process.communicate()
     if process.returncode == 0:
         subnets = json.loads(stdout.decode('utf-8'))
-        return [(subnet['name'], subnet['addressPrefix']) for subnet in subnets]
+        subnets_list = []
+        for subnet in subnets:
+            if 'addressPrefix' in subnet:
+                subnets_list.append((subnet['name'], subnet['addressPrefix']))
+            elif 'addressPrefixes' in subnet:
+                for prefix in subnet['addressPrefixes']:
+                    subnets_list.append((subnet['name'], prefix))
+        return subnets_list
     else:
         raise Exception("Error listing VNET subnets: " + stderr.decode('utf-8'))
 
 def find_subnet_for_ip(ip_address):
     subnets = get_vnet_subnets()
     ip = ipaddress.ip_address(ip_address)
-    for name, addressPrefix in subnets:
-        if ip in ipaddress.ip_network(addressPrefix):
-            return name, addressPrefix
-    return None, None
+    for name, prefix in subnets:
+        network = ipaddress.ip_network(prefix, strict=False)
+        if ip in network:
+            return name
+    return None
 
 def main():
-    ip_address = get_ip_address()
-    subnet_name, addressPrefix = find_subnet_for_ip(ip_address)
+    ip_address = sys.argv[1] if len(sys.argv) > 1 else get_ip_address()
+    subnet_name = find_subnet_for_ip(ip_address)
     if subnet_name:
         print(subnet_name)
     else:
-        print(None)
+        print("None")
 
 if __name__ == '__main__':
     main()
