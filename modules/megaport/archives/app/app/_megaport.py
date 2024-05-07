e1bebbe7f5df4146cd20393760fb924bb86dc76f
Kayode Salawu <salawu@gmail.com>
2024-05-07 22:21:13 +0000

    megaport api's 80%

--- modules/megaport/archives/app/app/_megaport.py
+++ modules/megaport/archives/app/app/_megaport.py
@@ -6,7 +6,7 @@ import os
 import json
 
 class Megaport:
-    def __init__(self, mcr_name, service_key=None, username=None, password=None):
+    def __init__(self, mcr_name=None, service_key=None, username=None, password=None):
         self.base_url = "https://api.megaport.com"
         self.auth_url = "https://auth-m2m.megaport.com/oauth2/token"
         self.username = os.getenv('TF_VAR_megaport_access_key')
@@ -14,21 +14,9 @@ class Megaport:
         if not self.username or not self.password:
             raise ValueError("Username and password must be provided via environment.")
         self.access_token = self.get_access_token()
-        self.mcr = self.get_mcr(mcr_name)
+        self.mcr_name = mcr_name
+        self.mcr = self.get_mcr()
         self.connections = None
-
-    def get_access_token(self):
-        auth_credentials = base64.b64encode(f"{self.username}:{self.password}".encode()).decode()
-        access_token_response = requests.post(self.auth_url, headers={
-            "Authorization": f"Basic {auth_credentials}",
-            "Content-Type": "application/x-www-form-urlencoded"
-        }, data={"grant_type": "client_credentials"})
-
-        access_token = access_token_response.json().get('access_token')
-        if not access_token:
-            sys.exit("MegaportAuth: Failed!")
-        print("MegaportAuth: Success!")
-        return access_token
 
     def list_mcrs(self, search_string=None):
         headers = {
@@ -45,7 +33,20 @@ class Megaport:
             else:
                 print(product.get("productName"))
 
-    def get_mcr(self, product_name):
+    def get_access_token(self):
+        auth_credentials = base64.b64encode(f"{self.username}:{self.password}".encode()).decode()
+        access_token_response = requests.post(self.auth_url, headers={
+            "Authorization": f"Basic {auth_credentials}",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }, data={"grant_type": "client_credentials"})
+
+        access_token = access_token_response.json().get('access_token')
+        if not access_token:
+            sys.exit("MegaportAuth: Failed!")
+        print("MegaportAuth: Success!")
+        return access_token
+
+    def get_mcr(self):
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.access_token}"
@@ -54,15 +55,14 @@ class Megaport:
         all_products = response.json().get("data")
 
         for product in all_products:
-            if product.get("productType") == "MCR2" and product.get("productName") == product_name:
-                self.mcr = product
+            if product.get("productType") == "MCR2" and product.get("productName") == self.mcr_name:
                 return product
 
     def get_connections(self):
         self.connections = self.mcr.get("associatedVxcs")
-        return (json.dumps(self.connections, indent=4))
+        return (self.connections)
 
-    def select_connection(self):
+    def prompt_connection(self):
         if not self.connections:
             self.get_connections()
         print("\nAvailable Connections:")
@@ -75,7 +75,7 @@ class Megaport:
         except (IndexError, ValueError):
             print("Invalid selection.")
 
-    def select_bgp_action(self):
+    def prompt_bgp_action(self):
         actions = ("1. Enable BGP", "2. Disable BGP")
         print("\nSelect BGP action:")
         for action in actions:
@@ -92,7 +92,7 @@ class Megaport:
             print("Invalid selection.")
 
     def interactive_bgp(self):
-        connection = self.select_connection()
+        connection = self.prompt_connection()
         product_uid = connection['productUid']
         csp_connection = connection['resources']['csp_connection']
 
@@ -108,7 +108,7 @@ class Megaport:
                     for bgpConnection in interface['bgpConnections']:
                         current_action = bgpConnection.get('shutdown')
                         print(f"(BGP shutdown is currently {current_action})")
-                        action = self.select_bgp_action()
+                        action = self.prompt_bgp_action()
                         if action == "enable":
                             bgpConnection['shutdown'] = False
                         elif action == "disable":
--- modules/megaport/archives/app/app/main.py
+++ modules/megaport/archives/app/app/main.py
@@ -1,72 +1,69 @@
 import argparse
 import sys
-import os
 from _megaport import *
 
-def get_service_key(resource_group, circuit_name):
-    service_key = os.popen(f"az network express-route show --resource-group {resource_group} --name {circuit_name} --query \"serviceKey\" -o tsv").read().strip()
-    if not service_key:
-        sys.exit("ServiceKey: Failed!")
-    print("ServiceKey: Success!")
-    return service_key
+parser = argparse.ArgumentParser(description="Manage MCR commands")
+parser.add_argument('command', nargs='*', help='The command to execute (e.g., "list mcr")')
+parser.add_argument('-g', '--resource-group', help='The name of the resource group.')
+parser.add_argument('-c', '--circuit-name', help='The name of the circuit.')
+parser.add_argument('-a', '--access-token', help='Get Megaport access token.')
+parser.add_argument('-m', '--mcr', help='Megaport MCR name.')
+parser.add_argument('-i', '--interactive', nargs='+', help='Interactive options such as MCR BGP, or VXC operations.')
 
-def list_mcrs(megaport):
-    megaport.list_mcrs()
+args = parser.parse_args()
+command_str = ' '.join(args.command)
+
+def list_mcrs(search_string=None):
+    megaport = Megaport()
+    megaport.list_mcrs(search_string)
 
 def list_connections(megaport):
     connections = megaport.get_connections()
-    print(connections)
+    for index, connection in enumerate(connections, start=1):
+        print(f"{index}. {connection.get('productName')}")
 
-def get_access_token(megaport):
+def get_access_token():
+    megaport = Megaport()
     print("Access Token: ", megaport.access_token)
 
 def show_mcr(megaport, product_name):
     mcr_details = megaport.get_mcr(product_name)
     if mcr_details:
-        print("MCR Details:", mcr_details)
+        print("MCR Details:", json.dumps(mcr_details, indent=2))
     else:
         print("MCR not found.")
 
 def interactive_bgp(megaport):
     megaport.interactive_bgp()
 
-parser = argparse.ArgumentParser(description="Manage MCR commands")
-parser.add_argument('command', nargs='?', help='The command to execute (e.g., "list mcr")')
-parser.add_argument('-g', '--resource-group', help='The name of the resource group.')
-parser.add_argument('-c', '--circuit-name', help='The name of the circuit.')
-parser.add_argument('-m', '--mcr', required=True, help='Megport MCR name.')
-parser.add_argument('-i', '--interactive', nargs='+', help='Interactive options such as BGP or VXC adjustments.')
-
-args = parser.parse_args()
-
-# service_key = get_service_key(args.resource_group, args.circuit_name)
-
 if args.interactive:
+    if not args.mcr:
+        print("Error: Interactive operations require -m parameter.")
+        sys.exit(1)
+    megaport = Megaport(args.mcr)
     for option in args.interactive:
         if option.lower() == "bgp":
-            megaport = Megaport(args.mcr)
             interactive_bgp(megaport)
         elif option.lower() == "vxc":
             print("VXC operation is not implemented yet.")
         else:
             print(f"Error: {option} is not a valid interactive option.")
-elif args.command:
-    if args.command == "list mcr":
-        if not (args.resource_group and args.circuit_name):
-            print("Error: list mcr requires -g and -c parameters.")
-            sys.exit(1)
-        megaport = Megaport(args.mcr)
-        list_mcrs(megaport)
-    elif args.command == "list connections":
-        if not (args.resource_group and args.circuit_name):
-            print("Error: list connections requires -g and -c parameters.")
+elif command_str:
+    if command_str == "list mcr":
+        list_mcrs()
+    elif command_str == "list connections":
+        if not args.mcr:
+            print("Error: list connections requires -m parameter.")
             sys.exit(1)
         megaport = Megaport(args.mcr)
         list_connections(megaport)
-    elif args.command == "get access token":
-        get_access_token(megaport)
-    elif args.command.startswith("show mcr"):
-        product_name = args.command.split(" ")[1] if len(args.command.split(" ")) > 1 else None
+    elif command_str == "get access-token":
+        get_access_token()
+    elif command_str.startswith("show mcr"):
+        if not args.mcr:
+            print("Error: show mcr requires -m parameter.")
+            sys.exit(1)
+        product_name = command_str.split(" ")[2] if len(command_str.split(" ")) > 2 else None
         if not product_name:
             print("Error: show mcr requires an MCR product name.")
             sys.exit(1)
