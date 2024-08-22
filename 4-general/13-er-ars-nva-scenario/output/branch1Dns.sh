01acc54d1c0be4d25400e3c4fff8a53b8e85d1ef
Kayode Salawu <salawu@gmail.com>
2024-08-22 07:40:17 +0000

    lab13 added

---
+++ 4-general/13-er-ars-nva-scenario/output/branch1Dns.sh
@@ -0,0 +1,89 @@
+#! /bin/bash
+
+# disable systemd-resolved as it conflicts with dnsmasq on port 53
+
+systemctl stop systemd-resolved
+systemctl disable systemd-resolved
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "$(hostname -I | cut -d' ' -f1) $(hostname)" >> /etc/hosts
+
+apt update
+apt install -y tcpdump dnsutils net-tools
+apt install -y unbound
+
+touch /etc/unbound/unbound.log
+chmod a+x /etc/unbound/unbound.log
+
+cat <<EOF > /etc/unbound/unbound.conf
+server:
+        port: 53
+        do-ip4: yes
+        do-ip6: yes
+        do-udp: yes
+        do-tcp: yes
+
+        interface: 0.0.0.0
+        interface: ::0
+
+        access-control: 0.0.0.0 deny
+        access-control: ::0 deny
+        access-control: 10.0.0.0/8 allow
+        access-control: 172.16.0.0/12 allow
+        access-control: 192.168.0.0/16 allow
+        access-control: 100.64.0.0/10 allow
+        access-control: 127.0.0.0/8 allow
+        access-control: 35.199.192.0/19 allow
+        access-control: fd00::/8 allow
+
+        # local data records
+        local-data: "branch1vm.corp 300 IN A 10.10.0.5"
+        local-data: "branch2vm.corp 300 IN A 10.20.0.5"
+        local-data: "branch3vm.corp 300 IN A 10.30.0.5"
+        local-data: "branch1vm.corp 300 IN AAAA fd00:db8:10::5"
+        local-data: "branch2vm.corp 300 IN AAAA fd00:db8:20::5"
+        local-data: "branch3vm.corp 300 IN AAAA fd00:db8:30::5"
+
+        # hosts redirected to PrivateLink
+
+
+forward-zone:
+        name: "eu.az.corp."
+        forward-addr: 10.11.8.4
+
+forward-zone:
+        name: "us.az.corp."
+        forward-addr: 10.22.8.4
+
+forward-zone:
+        name: "privatelink.blob.core.windows.net."
+        forward-addr: 10.11.8.4
+
+forward-zone:
+        name: "privatelink.azurewebsites.net."
+        forward-addr: 10.11.8.4
+
+forward-zone:
+        name: "privatelink.database.windows.net."
+        forward-addr: 10.11.8.4
+
+forward-zone:
+        name: "privatelink.table.cosmos.azure.com."
+        forward-addr: 10.11.8.4
+
+forward-zone:
+        name: "privatelink.queue.core.windows.net."
+        forward-addr: 10.11.8.4
+
+forward-zone:
+        name: "privatelink.file.core.windows.net."
+        forward-addr: 10.11.8.4
+
+forward-zone:
+        name: "."
+        forward-addr: 168.63.129.16
+EOF
+
+systemctl enable unbound
+systemctl restart unbound
+apt install resolvconf
+resolvconf -u
